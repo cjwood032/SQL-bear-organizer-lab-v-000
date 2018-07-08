e166041427dbcfaa6681e058df5ac702afb26a60
Christopher Wood <cjwood032@gmail.com>
2018-07-08 20:53:22 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,10 +6,7 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
   INSERT INTO bears VALUES (1,"Mr. Chocolate", 55, "M", "Black", "Salty", 0);
 INSERT INTO bears VALUES (2,"Rowdy", 43, "M", "Golden", "Stuffed", 0);
 INSERT INTO bears VALUES (3,"Tabitha", 3, "F", "dark brown", "annoyting", 1);
-INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "White", "Grizzled", 1);
-INSERT INTO bears VALUES (5,"Melissa", 23, "F", "dark brown", "drunk", 0);
-INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Green", "Jolly", 1);
-INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Orange"
+INSERT INTO bears VALU
 
 end
 
